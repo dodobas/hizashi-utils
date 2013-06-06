3cf32f989ed180d9b079f81a40b291e2764a5526
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2013-06-06 16:26:17 +0200

    Added initproject command

---
+++ hizashi-utils/bin/hizashi.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from django.core import management
+import argparse
+
+
+def initproject(args):
+    """
+    Initializes Django Hizashi project
+    """
+    print 'Initializing Django Hizashi project...'
+
+    management.call_command(
+        'startproject', args.project_name,
+        template=args.template,
+        extensions=['.py', '.rst'], verbosity=0)
+
+
+parser = argparse.ArgumentParser(description='Execute Hizashi utilities')
+subparsers = parser.add_subparsers(title='Subcommands')
+
+
+# initproject options
+parser_initproject = subparsers.add_parser(
+    'initproject', help='Initializes Django Hizashi project'
+)
+parser_initproject.add_argument(
+    'project_name', type=str, help='Django Project name')
+parser_initproject.add_argument(
+    '--template', type=str, help='Django Project name',
+    default='/home/dodobas/PROJEKTI/HIZASHI/hizashi-project-template/'
+)
+parser_initproject.set_defaults(func=initproject)
+
+if __name__ == "__main__":
+    # parse the args, and call default function
+    args = parser.parse_args()
+    args.func(args)
