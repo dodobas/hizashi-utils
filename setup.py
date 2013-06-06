ebe1092fafde21f33ca125dd268a5e629df17624
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2013-06-06 16:40:09 +0200

    Updated setup.py to use GIT plugin

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
-from distutils.core import setup
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
 
 setup(
     name='hizashi-utils',
     version='0.1.0',
     author='Dražen Odobašić',
     author_email='dodobasic@gmail.com',
-    packages=['hizashi-utils'],
+    packages=find_packages(),
+    include_package_data=True,
     scripts=['hizashi-utils/bin/hizashi.py'],
     url='',
     license='LICENSE.txt',
     description='Utils for managing Hizashi Django environment',
+    setup_requires=[
+        "setuptools_git >= 0.3"
+    ],
     install_requires=[
         "Django >= 1.5"
     ]
