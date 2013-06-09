6cbdeb2b48b1e607f3b549663664b69f89a4fc6d
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2013-06-09 14:07:29 +0200

    Updated setup.py, bumped project version to 0.2.0

--- setup.py
+++ setup.py
@@ -1,17 +1,33 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(
     name='hizashi-utils',
-    version='0.1.0',
+    version='0.2.0',
     author='Dražen Odobašić',
     author_email='dodobasic@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    scripts=['hizashi-utils/bin/hizashi.py'],
+    scripts=['hizashi_utils/bin/hizashi.py'],
     url='https://github.com/dodobas/hizashi-utils',
+    keywords='django hizashi utils helpers',
     license='LICENSE.txt',
-    description='Utils for managing Hizashi Django environment',
+    description='Utils for managing Django Hizashi environment',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities'
+    ],
     setup_requires=[
         "setuptools_git >= 0.3"
     ],
